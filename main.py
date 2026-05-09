9dde1c3943408bc758a90333661e7db36ba89e90
diomandiakite92-ux <diomandiakite92@gmail.com>
2026-05-09 16:43:10 +0200

    ajout du code client

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 """Entry point."""
 
-from models.card import Card
+from models.deck import Deck
+from controllers.base import Controller
+from views.base import View
+
+
+def main():
+    deck = Deck()
+    view = View()
+    game = Controller(deck, view)
+    game.run()
 
 
 if __name__ == "__main__":
-    card1 = Card("diamonds", "cinq")
-    card2 = Card("coeurs", "cinq")
-    card3 = Card("coeurs", "valet")
-    print(card1 < card2)
-    print(card3 > card2)
+    main()
